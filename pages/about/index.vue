98dc55f0dc28d8b295e445a9af2e4dc1285f45fd
Pedro <phfs28@gmail.com>
2021-08-29 17:02:40 -0300

    Added pages and styles

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>This is the About page!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
