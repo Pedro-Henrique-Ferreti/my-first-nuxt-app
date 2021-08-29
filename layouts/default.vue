e12896e1b91437ca78061a07cd6e357d05498c22
Pedro <phfs28@gmail.com>
2021-08-29 19:07:48 -0300

    Added header and sidenav components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,29 @@
 <template>
-  <nuxt />
+  <div>
+    <the-header @sidenavToggle="displaySidenav = !displaySidenav" />
+    <the-sidenav 
+      :show="displaySidenav"
+      @close="displaySidenav = false" />
+    <nuxt />
+  </div>
 </template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader.vue';
+import TheSidenav from '@/components/Navigation/TheSidenav.vue';
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav,
+  },
+  data() {
+    return {
+      displaySidenav: false,
+    }
+  }
+}
+</script>
 
 <style>
 html { 
